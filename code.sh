a2daca18f182fcc11c12347d722ed6537602b4dd
endlesssmp <azimpr2000@gmail.com>
2025-09-26 16:19:58 +0530

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Clone repo
+git clone https://github.com/titan-modz/ubuntu-console
+cd ubuntu-console
+
+# Update system
+apt update -y
+
+# Install dependencies
+apt install -y zip unzip npm docker.io
+
+# Unzip project (if needed)
+unzip -o ubuntu-cockpit-console.zip || true
+
+# Install Node packages
+npm install
+
+# Pull Ubuntu Docker image
+docker pull ubuntu:22.04
+
+# Start the server
+npm start
